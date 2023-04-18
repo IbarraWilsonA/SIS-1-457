d25e84adff5e3a66507624e4d13673a7201e8978
Wilson Ramiro Ibarra Alejandro <103014992+IbarraWilsonA@users.noreply.github.com>
2023-04-18 11:50:36 -0400

    Add files via upload

---
+++ factorial.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+class Factorial {
+public:
+    int n;
+	Factorial();
+	calcularFactorial();
+};
+Factorial::Factorial()
+{
+	n = 4;
+}
+int Factorial::calcularFactorial() 
+{
+	int factorialCalculado = 1;
+	for (int i=1; i<n; i++) 
+	{
+		factorialCalculado = factorialCalculado * i;
+	}
+	
+	return factorialCalculado;
+}
+int main() 
+{
+	Factorial factl;
+	int x = factl.calcularFactorial();
+	cout << x;
+	return 0;
+}
